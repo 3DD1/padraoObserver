93402af6957c9808d1203af1ff3a74287626cb12
Edmilson <scovinowork@gmail.com>
2021-10-09 14:58:38 -0300

    classe party

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,6 +1,23 @@
 import java.util.Observable;
 
 public class Party extends Observable {
+    private String nameParty;
+    private int  quantityPlayersInParty;
 
+    public Party(String nameParty, int quantityPlayersInParty) {
+        this.nameParty = nameParty;
+        this.quantityPlayersInParty = quantityPlayersInParty;
+    }
 
+    public void sendInviteParty(){
+        setChanged();
+        notifyObservers();
+    }
+    @Override
+    public String toString(){
+        return "Party{" +
+                " Name Party = "+ nameParty+
+                ", quantidy players = " + quantityPlayersInParty+
+                '}';
+    }
 }
