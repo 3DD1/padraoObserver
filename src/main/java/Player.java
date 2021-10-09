8f9ba65483f78018af53a3721d777f53a2af8872
Edmilson <scovinowork@gmail.com>
2021-10-09 14:52:40 -0300

    classes build

---
+++ src/main/java/Player.java
@@ -0,0 +1,24 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Player implements Observer {
+    private String gamerTag;
+    private int lvl;
+    private int kill;
+    private String friend;
+
+    public Player(String gamerTag){
+        this.gamerTag = gamerTag;
+    }
+
+    public String getFriend(){
+        return this.friend;
+    }
+    public void joinParty(Party party){
+        party.addObserver(this);
+
+    }
+    public void update(Observable party, Object arg1){
+        this.friend = this.gamerTag + ", your friend join the party" + party.toString();
+    }
+}
